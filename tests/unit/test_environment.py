94c76b945461b554a9ee4650261139161c5ad012
Rubén Pozo <ruben.pozo@tui.com>
2023-06-06 20:10:53 +0200

    only two dimwnsions

--- tests/unit/test_environment.py
+++ tests/unit/test_environment.py
@@ -5,10 +5,10 @@ class TestEnvironment:
 
     def test_environment(self):
         environment = Environment(
-            size=(50, 50, 50),
+            size=(50, 50),
             cycle_time=5
         )
         assert environment.cycle_time == 5
         assert environment.x == 50
         assert environment.y == 50
-        assert environment.z == 50
+
--- virtecoga/environment.py
+++ virtecoga/environment.py
@@ -6,17 +6,15 @@ from virtecoga.processor import Processor
 class Environment:
     x = 0
     y = 0
-    z = 0
     living_beings = []
     cycles = []
     cycle_time = 0
     running = False
     processor = None
 
-    def __init__(self, size=(100, 100, 100), cycle_time=1):
+    def __init__(self, size=(100, 100), cycle_time=1):
         self.x = size[0]
         self.y = size[1]
-        self.z = size[2]
         self.cycle_time = cycle_time
         self.processor = Processor(self)
 
